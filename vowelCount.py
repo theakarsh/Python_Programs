21dffad429e0887c652a685d4f90284b3d3421ec
Akarsh Singh <theakarshg@gmail.com>
2025-11-25 23:35:24 +0530

    Update repo

---
+++ vowelCount.py
@@ -0,0 +1,17 @@
+# Write a function that takes String as input & returns count of Vowel present in String.
+
+def vowelConsCount(userInput):
+    vowels = "aeiouAEIOU"
+    countVowel = 0
+    countConsonants = 0
+
+    for eachChar in userInput:
+        if eachChar.isalpha():
+            if eachChar in vowels:
+                countVowel += 1
+            else:
+                countConsonants += 1
+    return countVowel, countConsonants
+vowels, consonants = vowelConsCount("Akarsh Singh")
+print(f"Vowels present : {vowels}")
+print(f"Consonants present : {consonants}")
